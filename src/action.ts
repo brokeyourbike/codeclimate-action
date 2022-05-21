bd6608062e40655b2586dda1b8b01e49bfacdb73
Ivan Stasiuk <brokeyourbike@gmail.com>
2022-05-21 19:43:54 +0300

    feat: upload coverage

--- src/action.ts
+++ src/action.ts
@@ -4,20 +4,40 @@ import * as path from 'path'
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as tc from '@actions/tool-cache'
-import { parse } from './options'
+import * as github from '@actions/github'
 import { verifySignature } from './validate'
-import { getDownloadUrl, getFilename } from './helpers'
+import { isTrue, cleanRef, getDownloadUrl, getFilename, parseCoverageLocations } from './helpers'
 
 export async function run (): Promise<void> {
   try {
     const platform = os.platform()
-
     if (platform === 'win32') {
       core.setFailed('Reporter in not supported on windows')
       return
     }
 
-    const { execArgs, options, version } = parse()
+    const context = github.context
+
+    const token = core.getInput('token')
+    const locations = parseCoverageLocations(core.getInput('files'))
+    const prefix = core.getInput('prefix')
+    const verbose = isTrue(core.getInput('verbose'))
+    const workingDir = core.getInput('working-directory')
+    const version = core.getInput('version') !== '' ? core.getInput('version') : 'latest'
+
+    const execOptions: exec.ExecOptions = {
+      cwd: workingDir !== '' ? workingDir : undefined,
+      env: {
+        CC_TEST_REPORTER_ID: token,
+        GIT_BRANCH: cleanRef(process.env.GITHUB_REF ?? context.ref),
+        GIT_COMMIT_SHA: process.env.GITHUB_SHA ?? context.sha
+      }
+    }
+
+    if (locations.length <= 0) {
+      core.setFailed('No coverage locations found after parsing `files` input')
+      return
+    }
 
     core.info('Downloading reporter..')
     const filePath = await tc.downloadTool(
@@ -34,8 +54,31 @@ export async function run (): Promise<void> {
     core.info('Making it executable..')
     fs.chmodSync(filePath, 0o755)
 
-    core.info('Uploading coverage report..')
-    await exec.exec(filePath, execArgs, options)
+    core.info('Running before-build..')
+    let code: number = await exec.exec(filePath, ['before-build'], execOptions)
+    if (code !== 0) {
+      core.setFailed(`Coverage before-build exited with code ${code}`)
+      return
+    }
+
+    if (locations.length === 1) {
+      const afterBuildArgs: string[] = ['after-build', '--coverage-input-type', locations[0].format]
+      if (prefix !== '') {
+        afterBuildArgs.push('--prefix', prefix)
+      }
+      if (verbose) {
+        afterBuildArgs.push('--debug')
+      }
+      afterBuildArgs.push(locations[0].path)
+
+      code = await exec.exec(filePath, afterBuildArgs, execOptions)
+      if (code !== 0) {
+        core.setFailed(`Coverage after-build exited with code ${code}`)
+        return
+      }
+    } else {
+      //
+    }
   } catch (err) {
     const message = err instanceof Error ? err.message : 'Failed to upload coverage report'
     core.setFailed(`Encountered an unexpected error ${message}`)
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,8 @@
+export interface ICoverageLocation {
+  path: string
+  format: string
+}
+
 export const getFilename = (platform: string, version: string): string => {
   return `test-reporter-${version}-${platform}-amd64`
 }
@@ -16,4 +21,41 @@ export const getShasumSigUrl = (platform: string, version: string): string => {
 
 export const getPublicKeyDownloadUrl = (): string => {
   return 'https://keys.openpgp.org/vks/v1/by-fingerprint/9BD9E2DD46DA965A537E5B0A5CBF320243B6FD85'
+}
+
+/**
+ * Parses action input to determine is value is true.
+ */
+export const isTrue = (variable): boolean => {
+  const lowercase = variable.toLowerCase()
+  return (
+    lowercase === '1' ||
+    lowercase === 't' ||
+    lowercase === 'true' ||
+    lowercase === 'y' ||
+    lowercase === 'yes'
+  )
+}
+
+/**
+ * Removes 'refs/heads/' prefix.
+ */
+export const cleanRef = (ref: string): string => {
+  return ref.replace(/^refs\/heads\//, '')
+}
+
+export const parseCoverageLocations = (locationsPath: string): ICoverageLocation[] => {
+  const locations: ICoverageLocation[] = []
+
+  const lines = locationsPath.split(/\r?\n/).filter((x) => x).map((x) => x.trim())
+  for (const line of lines) {
+    const lineParts = line.split(':')
+    const format = lineParts.slice(-1)[0]
+    const path = lineParts.slice(0, -1)[0]
+
+    const location: ICoverageLocation = { path, format }
+    locations.push(location)
+  }
+
+  return locations
 }
